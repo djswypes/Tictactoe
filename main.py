1c4d8337c8061c9376a900e981e8549d3c96aff4
djswypes@gmail.com <83518572+djswypes@users.noreply.github.com>
2022-07-29 22:03:21 +0100

    error correction

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ class Board:
         ]
 
         self.winning_combinations = [
+            # Horizontal
             ['1', '2', '3'], 
             ['4', '5', '6'], 
             ['7', '8', '9'], 
+            # Vertical
             ['1', '4', '7'], 
             ['2', '5', '8'], 
             ['3', '6', '9'],
+            # Diagonal
             ['1', '5', '9'], 
-            ['3', '6', '9']
+            ['3', '5', '7']
             ]
 
     def draw_board(self):
